316036672d188843ef0e54d9b2cd6442f910d293
nsbkaratas <66446937+nsbkaratas@users.noreply.github.com>
2021-08-19 13:51:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const paper= document.querySelector("#paper")
 const scissors= document.querySelector("#scissors")
 const rock= document.querySelector("#rock")
 const userChoice = document.querySelector(".userChoice")
-let arr=['rock.png','paper.png','scissors.png']
+const reset=document.querySelector("#restart-button")
+const status= document.querySelector("#scoreBoard")
+const resButton = document.querySelector("#restart-button")
+const arr=['rock','paper','scissors']
+
 
 //display rock for user when selected
 rock.addEventListener("click", add_rock)
@@ -11,64 +15,76 @@ function add_rock() {
     img.src="images/rock.png"
 	document.getElementById('placehere').appendChild(img);
     let comp=compChoice() 
-    winnerRock()
-    console.log(winnerRock(img,comp))
-    console.log(img)
-    console.log(comp)
+    winnerRock(arr[0],comp)
+    console.log(winnerRock(arr[0],comp))
+    //reset.style.display="inline-block"
+    //status.style.display="inline-block"
+    //status.innerText=winnerRock(arr[0],comp)
 }
-//display rock for user when selected
+
+//display paper for user when selected
 paper.addEventListener("click", add_paper)
 function add_paper() { 
 	let img = document.createElement('img'); 
     img.src="images/paper.png"
 	document.getElementById('placehere').appendChild(img);
     let comp=compChoice()
-    winnerPaper()
-    console.log(winnerPaper(img,comp))
-    console.log(img)
-    console.log(comp)
+    winnerPaper(arr[1],comp)
+    console.log(winnerPaper(arr[1],comp))
+    reset.style.display="inline-block"
+    status.style.display="inline-block"
+    status.innerText=winnerPaper(arr[1],comp)
 }
-//display rock for user when selected
+//display scissors for user when selected
 scissors.addEventListener("click", add_scissors)
 function add_scissors() { 
 	let img = document.createElement('img'); 
     img.src="images/scissors.png"
 	document.getElementById('placehere').appendChild(img);
     let comp=compChoice()
-    winnerScissors()
-    console.log(winnerScissors(img,comp))
+    winnerScissors(arr[2],comp)
+    console.log(winnerScissors(arr[2],comp))
+    reset.style.display="inline-block"
+    status.style.display="inline-block"
+    status.innerText=winnerScissors(arr[2],comp)
+}
+
+//Restart button functionality
+resButton.addEventListener("click", restart)
+function restart(){   
+    window.location.reload()       
 }
 // function for computer random selection
 function compChoice(){
     let i=Math.floor(Math.random()*arr.length)
     let img=document.createElement('img')
-    let imgPath=arr[i]
-    img.src=`./images/${imgPath}`
+    let compPick=arr[i]
+    img.src=`./images/${compPick}.png`
     document.getElementById('placeForComp').append(img)
-    console.log(img)
+    return compPick
 }
 // case of player selection of rock
 function winnerRock(user,computer){
     if(user==computer){
         return "it's a tie!"
     }
-    else if(computer==="paper"){
-        return "Sorry you lost :("
+    else if(computer=== "scissors"){
+        return"Congragulations! You Win!" 
     }
     else{
-        return "Congragulations! You Win!"
+        return "Sorry you lost :("
     }
 }
 //case of player selection of paper
 function winnerPaper(user,computer){
-    if(user==computer){
+    if(user===computer){
         return "it's a tie!"
     }
-    else if(computer==="scissors"){
-        return "Sorry you lost :("
+    else if(computer==="rock"){
+        return  "Congragulations! You Win!"
     }
     else{
-        return "Congragulations! You Win!"
+        return "Sorry you lost :("       
     }
 }
 
@@ -77,36 +93,11 @@ function winnerScissors(user,computer){
     if(user==computer){
         return "it's a tie!"
     }
-    else if(computer==="rock"){
-        return "Sorry you lost :("
-    }
-    else{
+    else if(computer==="paper"){
         return "Congragulations! You Win!"
     }
+    else{
+        return "Sorry you lost :("
+    }
 }
-
-
-    // document.getElementById('placeForComp').src=`.images/${img}`
-
-
-
-// const handleClick=(e)=>{
-//     userChoice.restyle.display=e.target
-// //   const comp = compChoice()
-//     console.log(user)
-// }
-
-
-
-// choices.forEach(choice =>choice.addEventListener("click", handleClick))
-
-
-
-
-
-
-
-
-
-
-
+//resarting the game
